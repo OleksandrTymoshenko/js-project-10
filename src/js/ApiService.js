281c5d8cf9ab090eec9de77e5747312272103256
Igor Gladkiy <egunka1988@gmail.com>
2022-01-29 00:49:56 +0200

    add services and library

---
+++ src/js/ApiService.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: ''
+})
+
+export default class ApiService {
+
+    _apiKey = '4a69d9470951ad07e8f0cc655bb46705'
+
+    constructor() {
+        this.searchQuery = '',
+        this.page = 1
+    }
+
+    set query(newQuery) {
+        this.query = newQuery
+    }
+
+    get query() {
+        return this.searchQuery
+    }
+
+    async getPopularFilms() {
+        return await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${this._apiKey}`)
+            .then(response => response.json())
+            .then(data => data.results)
+            .catch(error => console.error(error))
+    }
+
+    async getFilmsByName() {
+        return await fetch(`https://api.themoviedb.org/3/search/movie?query=${this.searchQuery}&api_key=${this._apiKey}`)
+            .then(response => response.json())
+            .then(data => data.results)
+            .catch(error => console.error(error))
+    }
+
+    async getFilmDetails(id) {
+        return await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${this._apiKey}`)
+            .then(response => response.json())
+            .catch(error => console.error(error))
+    }
+
+
+
+
+}
---
+++ src/js/RenderService.js
@@ -0,0 +1,75 @@
+const list = document.querySelector('.list')
+const modal = document.querySelector('[data-modal]')
+const library = document.querySelector('[data-lib]')
+
+export default class RenderService {
+    constructor() {
+
+    }
+
+    renderAllFilms(filmArray) {
+        const markup = filmArray.map(({id, title, poster_path}) => {
+            const imagePath = `https://image.tmdb.org/t/p/w500/${poster_path}`
+            return `
+                    <li class="list__item" id=${id}>      
+                        <img class="item__img" src=${imagePath} alt="poster"> 
+                        <p class="item__title">${title}</p>
+                    </li>
+                `
+        }).join('')
+
+        list.insertAdjacentHTML('afterbegin', markup)
+    }
+
+    renderFilmDetails(film) {
+        modal.innerHTML = ''
+
+        const {id, poster_path, title, popularity, genres, overview} = film
+        const imagePath = `https://image.tmdb.org/t/p/w400/${poster_path}`
+        
+        const markup = `
+            <div class="film-details">
+
+                <div class="img-thumb" id=${id}>
+                    <img src=${imagePath} alt="poster" width="396">
+                </div>
+
+                <div class="about">
+                    <label class="about__label">
+                        Просмотров: 
+                        <p class="about__field popularity">${popularity}</p>
+                    </label>
+
+                    <label class="about__label">
+                        Фильм: 
+                        <p class="about__field title">${title}</p>
+                    </label>
+                    
+                    <label class="about__label">
+                        Жанр: 
+                        <p class="about__field genres">${genres[0]}</p>
+                    </label>
+
+                    <label class="about__label">
+                        О чём: 
+                        <p class="about__field overview">${overview}</p>
+                    </label>
+
+                    <div class="btn-block">
+                        <button class="add-to-lib-btn" data-action="addToLib">add to library</button>
+                        <button class="add-to-que-btn" data-action="addToQue">add to queue</button>
+                    </div>
+
+                </div>
+
+                <button class="close-btn" data-action="close">x</button>
+
+                
+            
+            </div>
+        `
+
+        modal.insertAdjacentHTML('afterbegin', markup)
+    }
+
+}
