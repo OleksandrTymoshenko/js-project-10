37b54c4251a2f88afd2f609eb671a179bfd2ed5c
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2022-01-30 23:16:32 +0200

    add generes-render function

--- src/index.js
+++ src/index.js
@@ -14,14 +14,13 @@ const refs = {
     naviListMain: document.querySelector('.navi__list[data-action="main"]'),
     naviListLib: document.querySelector('.navi__list[data-action="library"]'),
     headerMain: document.querySelector('header[data-action="main"]'),
-    headerLib: document.querySelector('header[data-action="library"]'),
-    
+    headerLib: document.querySelector('header[data-action="library"]'),    
 }
 
 refs.headerLib.style.display = "none";
 
-
 function onNaviListClick(e) {
+    apiService.getGeneresList()
     if(e.target.textContent === 'Home'){
         refs.headerMain.style.display = "block";
         refs.headerLib.style.display = "none";
@@ -34,8 +33,6 @@ function onNaviListClick(e) {
 
 refs.naviListMain.addEventListener('click', onNaviListClick) 
 refs.naviListLib.addEventListener('click', onNaviListClick) 
-
-
 
 const getPopular = () => {
     apiService.getPopularFilms().then(renderService.renderAllFilms)
