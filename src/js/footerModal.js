453e67507d3a501f1187f8bbd7442befca0244cd
AndriyBerezovskyi <dyus95@gmail.com>
2022-02-02 20:13:31 +0200

    members' photos added, minor fixes for footerModal.js

--- src/js/footerModal.js
+++ src/js/footerModal.js
@@ -1,22 +1,27 @@
 import {memberInfo} from './memberInfo';
 
 const footerBtnModal = document.querySelector('.footer__team-button')
+const closeModal = document.querySelector('.close-btn')
 const modal = document.querySelector('[data-modal]')
 const footerModal = document.querySelector('.member-list')
 
+footerBtnModal.addEventListener('click', onClickModalOpen)
+closeModal.addEventListener('click', onClickModalClose)
+
 
 function onClickModalOpen() {
-    modal.innerHTML = ''
     modal.classList.remove('hidden')
-    renderTeam();
+    modal.classList.remove('modal')
+    modal.classList.add('footer__modal')
+    createMarkup(memberInfo);
 }
 
 function createMarkup(memberInfo) {
-    return memberInfo.map(({ id, photo, name, role, linkedIn, gitHub, telegram }) => {
+   const markup = memberInfo.map(({ id, photo, name, role, linkedIn, gitHub, telegram }) => {
       return `<li class="member-item" id="${id}">
                 <div class="member-card">
                     <div class="member-photo">
-                        <img class="photo" src="${photo}" width="292" height="292" alt="team member">
+                        <img class="photo" src="${photo}" width="200" height="200" alt="team member">
                     </div>
                     <div class="member-description">
                         <p class="member-name">${name}</p>
@@ -25,23 +30,27 @@ function createMarkup(memberInfo) {
                     <div class="member-socials">
                         <ul class="socials-list">
                             <li class="socials-item">
-                                <a href="${linkedIn}">LinkedIn</a>
+                                <a class="socials-linkedin" href="${linkedIn}">LinkedIn</a>
                             </li>
                             <li class="socials-item">
-                                <a href="${gitHub}"> GitHub</a>
+                                <a class="socials-github" href="${gitHub}">GitHub</a>
                             </li>
                             <li class="socials-item">
-                                <a href="${telegram}">Telegram</a>
+                                <a class="socials-telegram" href="${telegram}">Telegram</a>
                             </li>
                         </ul>
                     </div>          
                 </div>        
             </li>`
     }).join("");
+    footerModal.insertAdjacentHTML('beforeend', markup)
+}     
+
+
+function onClickModalClose() {
+    modal.classList.add('hidden')
+    modal.classList.remove('footer__modal')
+    modal.classList.add('modal')
+    footerModal.innerHTML = ''
 }
 
-function renderTeam() {
-    footerModal.insertAdjacentHTML('beforeend', createMarkup(memberInfo))
-}      
-
-footerBtnModal.addEventListener('click', onClickModalOpen)
