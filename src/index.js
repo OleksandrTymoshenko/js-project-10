bdc58668dd83a92d2887954b4f4aa006692b3d7b
Igor Gladkiy <egunka1988@gmail.com>
2022-02-06 17:22:43 +0200

    infinity scroll
    only one page increment on scroll to end

--- src/index.js
+++ src/index.js
@@ -8,192 +8,183 @@ import { propFirebase } from './js/Auth';
 import './js/loader.js';
 import footerModal from './js/footerModal';
 import { openModal as openAuthModal } from './js/auth-modal';
-const apiService = new ApiService()
-const renderService = new RenderService()
+const apiService = new ApiService();
+const renderService = new RenderService();
 import Notiflix from 'notiflix';
 import './js/btn-up.js';
 
-
 const refs = {
-    input: document.querySelector('.input'),
-    list: document.querySelector('.list'),
-    modal: document.querySelector('[data-modal]'),
-    naviListMain: document.querySelector('.navi__list[data-action="main"]'),
-    naviListLib: document.querySelector('.navi__list[data-action="library"]'),
-    headerMain: document.querySelector('header[data-action="main"]'),
-    headerLib: document.querySelector('header[data-action="library"]'),
-    footerBtnModal: document.querySelector('.footer__team-button')    
-}
+  input: document.querySelector('.input'),
+  list: document.querySelector('.list'),
+  modal: document.querySelector('[data-modal]'),
+  naviListMain: document.querySelector('.navi__list[data-action="main"]'),
+  naviListLib: document.querySelector('.navi__list[data-action="library"]'),
+  headerMain: document.querySelector('header[data-action="main"]'),
+  headerLib: document.querySelector('header[data-action="library"]'),
+  footerBtnModal: document.querySelector('.footer__team-button'),
+};
 
-refs.headerLib.style.display = "none";
-refs.naviListMain.addEventListener('click', onNaviListClick) 
-refs.naviListLib.addEventListener('click', onNaviListClick) 
+refs.headerLib.style.display = 'none';
+refs.naviListMain.addEventListener('click', onNaviListClick);
+refs.naviListLib.addEventListener('click', onNaviListClick);
 
 function onNaviListClick(e) {
-    if (e.target.textContent === 'Home') {
-        refs.headerMain.style.display = "block";
-        refs.headerLib.style.display = "none";
-    }
-    if (e.target.textContent === 'My library' && isOnlain.logIn === true) {
-        refs.headerMain.style.display = "none";
-        refs.headerLib.style.display = "block";
-    }
+  if (e.target.textContent === 'Home') {
+    refs.headerMain.style.display = 'block';
+    refs.headerLib.style.display = 'none';
+  }
+  if (e.target.textContent === 'My library' && isOnlain.logIn === true) {
+    refs.headerMain.style.display = 'none';
+    refs.headerLib.style.display = 'block';
+  }
 }
 
 function getPopular() {
-    
-    window.addEventListener('scroll', debounce(onScroll, 1000) )
-    apiService.getPopularFilms().then(renderService.renderAllFilms)
-
+  apiService.getPopularFilms().then(films => {
+    renderService.renderAllFilms(films);
+    window.addEventListener('scroll', debounce(onScroll, 1500));
+  });
 }
 
-function closeModal  ()  {
-    refs.modal.classList.add('hidden')  
-    renderService.clearList()
+function onScroll() {
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+
+  const scrolled = window.scrollY;
+
+  const threshold = height - screenHeight / 4;
+
+  const position = scrolled + screenHeight;
+
+  if (position >= threshold) {
+    apiService.getPopularFilms().then(films => {
+      renderService.renderAllFilms(films);
+    });
+  }
+
+  window.removeEventListener('scroll', debounce(onScroll, 1500));
 }
 
+function closeModal() {
+  refs.modal.classList.add('hidden');
+  renderService.clearList();
+}
 
 function writeUserData(object) {
   const db = getDatabase();
   set(ref(db, `${Uid.uid}`), {
-      wathed: object,
+    wathed: object,
   });
-    // console.log(propFirebase.uid)
+  // console.log(propFirebase.uid)
 }
+
 function writeUserData(queue) {
   const db = getDatabase();
-  set(ref(db,  `${Uid.uid}`), {
-      queue: queue,
+  set(ref(db, `${Uid.uid}`), {
+    queue: queue,
   });
 }
 
-
 function EscCloseModal(e) {
-if (e.code === 'Escape') {
+  if (e.code === 'Escape') {
     closeModal();
-    window.removeEventListener('keydown', EscCloseModal)
+    window.removeEventListener('keydown', EscCloseModal);
   }
 }
 
-const openModal = (id,object,queue) => {
-    
-    refs.modal.classList.remove('hidden')
+const openModal = (id, object, queue) => {
+  refs.modal.classList.remove('hidden');
 
-    apiService.getFilmDetails(id).then(renderService.renderFilmDetails)
+  apiService.getFilmDetails(id).then(renderService.renderFilmDetails);
 
-    window.addEventListener('keydown', EscCloseModal)
+  window.addEventListener('keydown', EscCloseModal);
 
-    refs.modal.addEventListener('click', e => {
-        if (e.target.dataset.action === 'close') {
-            closeModal()
-        }
+  refs.modal.addEventListener('click', e => {
+    if (e.target.dataset.action === 'close') {
+      closeModal();
+    }
 
-        if (e.target.dataset.action === 'addToLib') {
+    if (e.target.dataset.action === 'addToLib') {
+      if (Uid.uid !== true) {
+        openAuthModal();
+        return;
+      }
 
-            if (Uid.logIn !== true) {
-                openAuthModal();
-                return;
-            }
+      const filmElem = document.querySelector('.film-details');
 
-           const filmElem = document.querySelector('.film-details')
-           
-            const obj = {
-                id: filmElem.id,
-                title: filmElem.querySelector('.about__title').innerText,
-                overview: filmElem.querySelector('.about__description--text').innerText,
-                path: filmElem.querySelector('.film-details__path').getAttribute('src'),
-                popularity: filmElem.querySelector('.popularity').innerText,
-            }
-            object = obj;
-            
-            writeUserData(object)
-            console.log(Uid.uid)
-        }
-    })
-       refs.modal.addEventListener('click', e => {
-        if (e.target.dataset.action === 'close') {
-            closeModal()
-        }
+      const obj = {
+        id: filmElem.id,
+        title: filmElem.querySelector('.about__title').innerText,
+        overview: filmElem.querySelector('.about__description--text').innerText,
+        path: filmElem.querySelector('.film-details__path').getAttribute('src'),
+        popularity: filmElem.querySelector('.popularity').innerText,
+      };
+      object = obj;
 
-        if (e.target.dataset.action === 'addToQue') {
+      writeUserData(object);
+      console.log(Uid.uid);
+    }
 
-            if (Uid.logIn !== true) {
-                openAuthModal();
-                return;
-            }
+    if (e.target.dataset.action === 'addToQue') {
+      if (Uid.logIn !== true) {
+        openAuthModal();
+        return;
+      }
 
-           const filmElem = document.querySelector('.film-details')
-           
-            const obj = {
-                id: filmElem.id,
-                title: filmElem.querySelector('.about__title').innerText,
-                overview: filmElem.querySelector('.about__description--text').innerText,
-                path: filmElem.querySelector('.film-details__path').getAttribute('src'),
-                popularity: filmElem.querySelector('.popularity').innerText,
-            }
-            queue = obj;
-            
-            writeUserData(queue)
-            console.log(Uid)
-        }
-   })
-}  
+      const filmElem = document.querySelector('.film-details');
 
-const getDetails = (e) => {
+      const obj = {
+        id: filmElem.id,
+        title: filmElem.querySelector('.about__title').innerText,
+        overview: filmElem.querySelector('.about__description--text').innerText,
+        path: filmElem.querySelector('.film-details__path').getAttribute('src'),
+        popularity: filmElem.querySelector('.popularity').innerText,
+      };
+      queue = obj;
 
-    if (e.target.nodeName === 'IMG') {
-            const { id } = e.target.parentNode
-            openModal(id)
-        }
-}
+      writeUserData(queue);
+      console.log(Uid);
+    }
+  });
+};
+
+const getDetails = e => {
+  if (e.target.nodeName === 'IMG') {
+    const { id } = e.target.parentNode;
+    openModal(id);
+  }
+};
 
 const findFilm = debounce(() => {
-    apiService.query = refs.input.value.trim()
-    
-    if (apiService.query.length >= 2) {
-        apiService.getFilmsByName().then(filmsArr => {
-            if (filmsArr.length === 0) {
-               return Notiflix.Notify.warning('Search result not successful. Enter the correct movie name')
-            }
+  apiService.query = refs.input.value.trim();
 
-            renderService.renderAllFilms(filmsArr)
-        })
-    }
-    
-}, 500)
+  if (apiService.query.length >= 2) {
+    apiService.getFilmsByName().then(filmsArr => {
+      if (filmsArr.length === 0) {
+        return Notiflix.Notify.warning(
+          'Search result not successful. Enter the correct movie name',
+        );
+      }
 
-function getMembers () {
-    refs.modal.classList.remove('hidden')
-    renderService.renderMembers()
-    const list = document.querySelector('.member-list')
+      renderService.renderAllFilms(filmsArr);
+    });
+  }
+}, 500);
+
+function getMembers() {
+  refs.modal.classList.remove('hidden');
+  renderService.renderMembers();
+  const list = document.querySelector('.member-list');
 }
 
-window.addEventListener('load', getPopular)
-refs.list.addEventListener('click', getDetails)
-refs.input.addEventListener('input',  findFilm)
-refs.footerBtnModal.addEventListener('click', getMembers)
-
-function  onScroll() {  
-    const height = document.body.offsetHeight
-    const screenHeight = window.innerHeight
-  
-    const scrolled = window.scrollY
-  
-    const threshold = height - screenHeight / 4
-  
-    const position = scrolled + screenHeight
-  
-      if (position >= threshold) {
-        apiService.incrementPage()
-        getPopular()
-        window.removeEventListener('scroll', onScroll )
-    }
-  }
+window.addEventListener('load', getPopular);
+refs.list.addEventListener('click', getDetails);
+refs.input.addEventListener('input', findFilm);
+refs.footerBtnModal.addEventListener('click', getMembers);
 
 function onNaviHomeClick() {
-        refs.headerMain.style.display = "none";
-    refs.headerLib.style.display = "block";
-    
+  refs.headerMain.style.display = 'none';
+  refs.headerLib.style.display = 'block';
 }
 
 export { onNaviHomeClick };
